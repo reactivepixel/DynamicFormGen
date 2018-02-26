8d79a4d2bc41c057a067c8017abb368c9e3cf527
LevanceWam <lkwamley@fullsail.edu>
2018-02-26 15:48:02 -0500

    Made the index.js and about to start working on the tool.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const array = [{
+  type: 'label',
+  for: 'nameInput',
+}, {
+  type: 'text',
+  for: 'firstNameInput',
+  required: true,
+}, {
+  type: 'text',
+  for: 'middleNameInput',
+  required: true,
+}, {
+  type: 'text',
+  for: 'LastNameInput',
+  required: true,
+}, {
+  type: 'text',
+  for: 'hobbies',
+  required: true,
+}, {
+  type: 'Submit',
+  for: 'submitButtom',
+  value: 'Submit Form',
+}];
