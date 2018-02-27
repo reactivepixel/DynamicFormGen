80cf320e28333d31db4f4a8f8df17e0368af7bc2
BHess2653 <GM_Dahaka@BHMac.local>
2018-02-26 21:37:48 -0500

    case password

--- src/tool.js
+++ src/tool.js
@@ -10,6 +10,22 @@ exports.formGen = (formInputs) => {
           output += input.for ? `for="${input.for}" ` : '';
           output += input.text ? `>${input.text}</label>` : '></label>';
           break;
+
+        case 'text':
+          output += `<input type="${input.type}" `;
+          output += input.id ? `id="${input.id}" ` : '';
+          output += input.placeholder ? `placeholder="${input.placeholder}" ` : '';
+          output += input.required ? 'required' : '';
+          output += '/>';
+          break;
+
+        case 'password':
+          output += `<input type="${input.type}" `;
+          output += input.id ? `id="${input.id}" ` : '';
+          output += input.placeholder ? `placeholder="${input.placeholder}" ` : '';
+          output += input.required ? 'required' : '';
+          output += '/>';
+          break;
         default:
 
       }
