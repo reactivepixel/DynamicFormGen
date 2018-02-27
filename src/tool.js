982dc0b7c3401d0c3db1829aef23510d6a18b6b5
BHess2653 <GM_Dahaka@BHMac.local>
2018-02-26 21:35:20 -0500

    case label

--- src/tool.js
+++ src/tool.js
@@ -4,8 +4,11 @@ exports.formGen = (formInputs) => {
   formInputs.forEach((input) => {
     if (input.type) {
       switch (input.type.toLowerCase()) {
-        case expression:
-
+        case 'label':
+          output += '<label ';
+          output += input.id ? `id="${input.id}" ` : '';
+          output += input.for ? `for="${input.for}" ` : '';
+          output += input.text ? `>${input.text}</label>` : '></label>';
           break;
         default:
 
