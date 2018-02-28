03cbc497abe738dc47e16289932f9aa78e0e2e6b
LevanceWam <lkwamley@fullsail.edu>
2018-02-28 15:43:05 -0500

    added a lot of packages and eslintrc

--- src/tools.js
+++ src/tools.js
@@ -1,37 +1,31 @@
 const formData = require('./index');
 
-let formString = ""; // makes a string to concat with
-if (formData){
-
-
+let formString = ''; // makes a string to concat with
+if (formData) {
   // loops through all the objects in the input array
-  formData.forEach( (data)=>{
+  formData.forEach((data) => {
     // data is a single input object
 
     // check on each type of input
-    if(data.type === 'label') {
+    if (data.type === 'label') {
       // console.log('it is a', data.type);
       // add to the formString
-      formString += `<label for='${data.for}'/>`
-
+      formString += `<label for='${data.for}'/>`;
     } else if (data.type === 'text') {
       // console.log('it is a', data.type);
 
-      formString += `<input type='${data.type}' name='${data.for}'/>`
+      formString += `<input type='${data.type}' name='${data.for}'/>`;
       // add to the formString
     } else if (data.type === 'submit') {
       // console.log('it is a', data.type);
 
-      formString += `<input type='${data.type}' name='${data.for}' value='${data.value}'/>`
+      formString += `<input type='${data.type}' name='${data.for}' value='${data.value}'/>`;
       // add to the formString
     }
-
-
-
   });
 
   // Return entire string
   console.log('the entire string', formString);
-}else{
+} else {
   console.log('You broke me');
 }
